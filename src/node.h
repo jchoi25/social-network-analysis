4627d07e24657c66e22608d7b91b2dea7b0aa859
soohar218 <soohar218@gmail.com>
2022-04-17 03:06:35 -0500

    node added

--- src/node.h
+++ src/node.h
@@ -1,1 +1,30 @@
 #pragma once
+
+class Node {
+
+    public:
+        Node(int id);
+
+        void setImportance(double importance);
+        void setBetweenness(double betweenness);
+        void setImportanceRank(unsigned importanceRank);
+        void setBetweennessRank(unsigned betweennessRank);
+        void increaseImportance()
+        void increaseBetweenness()
+
+        int getId() const;
+        double getImportance() const;
+        double getBetweenness() const;
+
+        unsigned getImportanceRank() const;
+        unsigned getBetweennessRank() const;
+
+    private:
+        int id_;
+        double importance_;
+        double betweenness_;
+
+        unsigned importanceRank_;
+        unsigned betweennessRank_;
+};
+
