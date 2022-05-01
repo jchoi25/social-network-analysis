790bdcf05f72c4c196545411587270d54782967d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 03:46:02 -0500

    pagerank done

--- src/pagerank.cpp
+++ src/pagerank.cpp
@@ -1,1 +1,24 @@
 #include "pagerank.h"
+
+pagerank::pagerank(Adjlist adjs) {
+    adjs_ = adjs;
+    num_nodes = adjs_.size();
+    probabilities.resize(num_nodes);
+}
+
+void pagerank::updateprobabilities() {
+    unsigned defaultvalue = 1 / num_nodes;
+    for (unsigned i = 0; i < adjs_.size(); ++i) {
+        unsigned total = 0;
+        for (Node* temp : adjs_[i]) {
+            unsigned idx = temp->get_id();
+            unsigned factor = adjs_[idx].size();
+            total += defaultvalue / factor;
+        }
+        probabilities[i] = total;
+    }
+}
+
+std::vector<unsigned> pagerank::get_probabilities() {
+    return probabilities;
+}
--- src/pagerank.h
+++ src/pagerank.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <vector>
+
+#include "node.h"
+
+typedef std::vector<std::vector<Node*>> Adjlist;
+
+class pagerank {
+  public:
+    pagerank(Adjlist adjs);
+    void updateprobabilities();
+    std::vector<unsigned> get_probabilities();
+
+  private:
+    Adjlist adjs_;
+    unsigned num_nodes;
+    std::vector<unsigned> probabilities;
+};
