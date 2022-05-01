4f63640863b6c1ef8e271d47b204db2d15491736
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 05:22:08 -0500

    betweenneess prototype

--- src/betweenness.cpp
+++ src/betweenness.cpp
@@ -1,1 +1,38 @@
 #include "betweenness.h"
+
+Betweenness::Betweenness(const Adjlist adjs) {
+    adjs_ = adjs;
+    betweennesses_.resize(adjs_.size());
+    betweennesses_.clear();
+}
+
+void Betweenness::calculateBetweenness() {
+    for (unsigned i = 0; i < adjs_.size(); ++i) {
+        std::queue<unsigned> q;
+        std::vector<bool> visited(adjs_.size());
+        std::vector<unsigned> backtrace(adjs_.size());
+        backtrace[i] = i;
+        for (bool v : visited) {
+            v = false;
+        }
+        q.push(i);
+        visited[i] = true;
+        while (!q.empty()) {
+            unsigned temp = q.front();
+            q.pop();
+            for (Node* adj : adjs_[temp]) {
+                unsigned id = adj->get_id();
+                if (!visited[id]) {
+                    q.push(id);
+                    visited[id] = true;
+                    backtrace[id] = temp;
+                }
+            }
+        }
+        for (unsigned b : backtrace) {
+            if (b != i) {
+                betweennesses_[b] += 1;
+            }
+        }
+    }
+}
--- src/betweenness.h
+++ src/betweenness.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <map>
+#include <queue>
+#include <vector>
+
+#include "node.h"
+
+typedef std::vector<std::vector<Node*>> Adjlist;
+
+class Betweenness {
+  public:
+    Betweenness(const Adjlist adjs_);
+    void calculateBetweenness();
+
+  private:
+    std::vector<unsigned> betweennesses_;
+    Adjlist adjs_;
+};
