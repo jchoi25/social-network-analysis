587e52e66dc182cfaa2fede7d74294fd2b6d5254
BuildTools <unconfigured@null.spigotmc.org>
2022-05-03 05:19:15 -0500

    pagerank test cases and fixed

--- src/pagerank.cpp
+++ src/pagerank.cpp
@@ -4,21 +4,58 @@ pagerank::pagerank(Adjlist adjs) {
     adjs_ = adjs;
     num_nodes = adjs_.size();
     probabilities.resize(num_nodes);
+    updateprobabilities();
 }
 
 void pagerank::updateprobabilities() {
-    unsigned defaultvalue = 1 / num_nodes;
+    double defaultvalue = 1.00 / num_nodes;
+
     for (unsigned i = 0; i < adjs_.size(); ++i) {
-        unsigned total = 0;
+        double total = 0;
         for (Node* temp : adjs_[i]) {
             unsigned idx = temp->get_id();
-            unsigned factor = adjs_[idx].size();
+            double factor = adjs_[idx].size();
             total += defaultvalue / factor;
         }
         probabilities[i] = total;
     }
 }
 
-std::vector<unsigned> pagerank::get_probabilities() {
+std::vector<double> pagerank::get_probabilities() {
     return probabilities;
+}
+
+std::vector<unsigned> pagerank::get_highest_rank() {
+    double highest = 0;
+    for (unsigned i = 0; i < probabilities.size(); ++i) {
+        if (probabilities[i] > probabilities[highest]) {
+            highest = i;
+        }
+    }
+
+    std::vector<unsigned> highests;
+    for (unsigned i = 0; i < probabilities.size(); ++i) {
+        if (probabilities[i] == probabilities[highest]) {
+            highests.push_back(i);
+        }
+    }
+
+    return highests;
+}
+
+std::vector<unsigned> pagerank::get_lowest_rank() {
+    double lowest = 0;
+    for (unsigned i = 0; i < probabilities.size(); ++i) {
+        if (probabilities[i] < probabilities[lowest]) {
+            lowest = i;
+        }
+    }
+    std::vector<unsigned> lowests;
+    for (unsigned i = 0; i < probabilities.size(); ++i) {
+        if (probabilities[i] == probabilities[lowest]) {
+            lowests.push_back(i);
+        }
+    }
+
+    return lowests;
 }
